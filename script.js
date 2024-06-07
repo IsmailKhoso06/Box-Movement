a0753ec4a4947578c5dfec30e23f01bcb1ca6f8b
Ismail <mismailkhoso006@gmailcom>
2024-06-07 10:55:47 +0500

    Box Movement JQuery

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    // $('#btn01').click(function() {
+    //   $('#box').removeClass('.moveBox');
+    // });
+    // $('#btn01').click(function() {
+    //     $('#box').addClass('.moveBox');
+    //   });
+    $('#btn01').click(function() {
+      $('#box').toggleClass('moveBox');
+    });
+  });
+  
